dbc9c2f09b0397ed5e0b9c774c84abc7d0685448
Zain-Ali <zain.ali@invozone.com>
2022-12-25 04:08:59 +0500

    Minor Updates

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const axios = require("axios");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const Trello = require("trello-events");
 const port = process.env.PORT || 8080;
-const trelloApiKey = process.env.TRELLO_KEY;
-const trelloToken = process.env.TRELLO_TOKEN;
 
 // For Locally use of https cert instead of http
 const fs = require("fs");
@@ -16,7 +13,7 @@ const key = fs.readFileSync("localhost-key.pem", "utf-8");
 const cert = fs.readFileSync("localhost.pem", "utf-8");
 
 const interactivity = require("./slack").interactivityTest;
-
+const trelloIntegration = require("./trello");
 const server = https.createServer({ key, cert }, app);
 
 // Slack live-events setup!.
@@ -28,7 +25,6 @@ app.use("/slack/events", slackEvents.requestListener());
 slackEvents.on('message', (event) => {
   console.log(event);
 });
-
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -42,17 +38,7 @@ app.get("/", (req, res) => {
 
 app.post("/interactivity", interactivity);
 
-const trello = new Trello({
-    pollFrequency: 1000 * 60,
-    minId: 0,
-    start: true,
-    trello: {
-        boards: ['63a2a0e50efce800a6863706', '63a3005f026382044d778b41'],
-        key: trelloApiKey,
-        token: trelloToken,
-    },
-});
-app.set('trello', trello);
+trelloIntegration();
 
 server.listen(port, () => {
     console.log(`App is listening at ${port}`);
--- trello.js
+++ trello.js
@@ -1,21 +1,38 @@
-const trello = req.app.get("trello");
+const Trello = require("trello-events");
+const trelloApiKey = process.env.TRELLO_KEY;
+const trelloToken = process.env.TRELLO_TOKEN;
 
-trello.on('updateCard', function (event, boardId) {
-    console.log("This is updateCard event", event);
-});
+module.exports = () => {
 
-trello.on('addMemberToBoard', function (event, boardId) {
-    console.log("This is addMemberToBoard event", event);
-});
+    const trello = new Trello({
+        pollFrequency: 1000 * 60,
+        minId: 0,
+        start: true,
+        trello: {
+            boards: ['63a2a0e50efce800a6863706', '63a3005f026382044d778b41'],
+            key: trelloApiKey,
+            token: trelloToken,
+        },
+    });
 
-trello.on('addCommentToCard', function (event, boardId) {
-    console.log("This is addCommentToCard event", event);
-});
+    trello.on('updateCard', function (event, boardId) {
+        console.log("This is updateCard event", event);
+    });
 
-trello.on('addMemberToCard', function (event, boardId) {
-    console.log("This is addMemberToCard event", event);
-});
+    trello.on('addMemberToBoard', function (event, boardId) {
+        console.log("This is addMemberToBoard event", event);
+    });
 
-trello.on('removeMemberFromCard', function (event, boardId) {
-    console.log("This is removeMemberFromCard event", event);
-});
+    trello.on('addCommentToCard', function (event, boardId) {
+        console.log("This is addCommentToCard event", event);
+    });
+
+    trello.on('addMemberToCard', function (event, boardId) {
+        console.log("This is addMemberToCard event", event);
+    });
+
+    trello.on('removeMemberFromCard', function (event, boardId) {
+        console.log("This is removeMemberFromCard event", event);
+    });
+
+}
